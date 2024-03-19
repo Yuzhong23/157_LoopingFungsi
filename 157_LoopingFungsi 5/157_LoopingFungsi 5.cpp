6eef1a4bdaa43ecc240acfcacda8055abefe8ca5
Yuzhong23 <xixixixifatur@gmail.com>
2024-03-19 11:51:04 +0700

    Membuat int sisi, input data, hitung luas, display, main,

---
+++ 157_LoopingFungsi 5/157_LoopingFungsi 5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sisi;
+
+void inputdata() {
+
+}
+int hitungluas() {
+ 
+}
+void display() {
+ 
+}
+int main() {
+
+    inputdata();
+    display();
+}
