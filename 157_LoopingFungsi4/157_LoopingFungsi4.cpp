5f3676e716ae2f4624bf83984ae241118d153fe1
Yuzhong23 <xixixixifatur@gmail.com>
2024-03-19 11:41:53 +0700

    Membuat void inputdata

--- 157_LoopingFungsi4/157_LoopingFungsi4.cpp
+++ 157_LoopingFungsi4/157_LoopingFungsi4.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int panjang, lebar;
 
+void inputdata() {
 
+    cout << "Masukan Nilai Panjang :";
+    cin >> panjang;
+    cout << "Masukan Nilai Lebar: ";
+    cin >> lebar;
+}
 
 int main() {
  
